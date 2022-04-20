960554472f1e581921fda4b7e308478bd78d80e3
Dennis Luangraj <dennisluangraj@gmail.com>
2022-04-20 10:18:46 -0400

    second commit

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 # give access to the project in any OS we find ourselves in
 # allow outside files/folders to be added to the project from
@@ -12,7 +14,8 @@ class Config():
     Using Environment variables where available otherwise
     Create the config variable if not done already.
     """
-
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or "You will never guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlife:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEPLOY_DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
